c4dd75d66094fe3d012a87ff382d2e80cbf61681
claudiosobral <claudio.sobral@exsiliconlife.ai>
2022-05-12 15:06:09 -0300

    add LD_PRELOAD

--- edge_run.sh
+++ edge_run.sh
@@ -1,5 +1,6 @@
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib/:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export CUDA_HOME=/usr/local/cuda
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1
 
 python3 src/call_flow.py --monitor
